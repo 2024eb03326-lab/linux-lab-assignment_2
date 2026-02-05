790b60958707724b1d55f02afab7e4b208967bc5
2024eb03326-lab <2024eb03326@online.bits-pilani.ac.in>
2026-02-05 16:19:00 +0300

    Add files via upload

---
+++ Q1/analyze.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "Error: Provide exactly one argument."
+    exit 1
+fi
+
+if [ -f "$1" ]; then
+    echo "File analysis:"
+    wc "$1"
+elif [ -d "$1" ]; then
+    echo "Directory analysis:"
+    echo "Total files: $(find "$1" -type f | wc -l)"
+    echo "Text files: $(find "$1" -type f -name "*.txt" | wc -l)"
+else
+    echo "Error: Path does not exist."
+fi
+
